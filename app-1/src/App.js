e10b0d3e5ab77f16bd9cb2a4e11022c21e3a704c
lebrowley <brocious123@gmail.com>
2020-05-13 21:08:16 -0600

    finished

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -27,20 +26,10 @@ class App extends Component {
 
 export default App;
 
-/*writing out the arrow function in the input onChange
-onChange= {function(e){
-  this.handleChange(e.target.value)
-}}
+// why do you have to pass in an argument (the e.target.value) when it is rendered? why couldn't you just call the function in the render
+// () => this.handleChange() which would then start up handleChange in the object and then pass in your argument so it would look like
+// handleChange(e.target.value){
+//   this.setState({message: e.target.value})
+// }
+// in your object??
 
-so this is an event that upon a change (enacted by the user) the function/method called handleChange on our 
-App class gets fired with the value on the target object which is also an object on the event object gets passed in
-
-why do you have to pass in an argument (the e.target.value) when it is rendered? why couldn't you just call the function in the render
-() => this.handleChange() which would then start up handleChange in the object and then pass in your argument so it would look like
-handleChange(e.target.value){
-  this.setState({message: e.target.value})
-}
-in your object
-
-is onChange a prop?? or an event handler? is there a difference? 
-*/
