7860bebc719264a1c44c9448b54deca3f390afb1
lebrowley <brocious123@gmail.com>
2020-05-13 21:15:06 -0600

    finished

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,5 +1,3 @@
-//what is axios?? 
-
 import React, { Component } from 'react';
 import './App.css';
 
@@ -38,27 +36,6 @@ export default App;
 //when to know to put things in the render method vs making a whole new custom method on the object
 //as in where is the best place to solve the "toy problem" bit? in the render? in a custom method....?
 //can jsx only go in the render method?? could a custom method have jsx too? or is it best practice to put jsx stuff in the render to keep things organized and good flow? 
-//and then knowing what to put in the return vs the render.....
-//you can chain on array methods???..... mind blown 
+//and then knowing what to put in the return vs the render....
+
 //what is the key={index} on the h2 doing??
-
-// handleChange(value){
-//   let words = this.state.words;
-//   let filteredWords = []
-
-//   for(let i=0; i < words.length; i++){
-//     if(words[i].includes(value)){
-//       filteredWords.push(words[i])
-//     }
-//   }
-//   this.setState({filteredWords: filteredWords})
-// }
-
-// render(){
-//   return(
-//     <div className='App'>
-//       <input onChange={e => this.handleChange(e.target.value)} type='text'/>
-//       <p>{this.state.filteredWords}</p>
-//     </div>
-//   )
-// }
